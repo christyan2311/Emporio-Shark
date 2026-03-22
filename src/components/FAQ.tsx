a3348239c3b21ea924ce2f8ef95db48716a1560d
Christyan Roberto Silva <christyanroberto31@gmail.com>
2026-03-21 21:36:49 -0300

    Ajuste FAQ

--- src/components/FAQ.tsx
+++ src/components/FAQ.tsx
@@ -3,7 +3,7 @@ import { useState } from 'react';
 import WhatsAppButton from './WhatsAppButton';
 
 export default function FAQ() {
-  const [openIndex, setOpenIndex] = useState<number | null>(0);
+  const [openIndex, setOpenIndex] = useState<number | null>(null);
 
   const faqs = [
     {
